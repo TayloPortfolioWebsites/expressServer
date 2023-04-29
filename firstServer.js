f00bd77643c9d5070e6231f09d5fd39ca3267685
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-29 11:13:02 +0800

    add contact route

--- firstServer.js
+++ firstServer.js
@@ -2,4 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/',function(request,response){
+    console.log(request);
+    console.log(typeof request);
+    response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/contact',function(req,res){
+    res.send("Hi, please contact me at : 12345@gmail.com");
+});
+
+app.listen(3000,function(){
+    console.log('Server is running on Port 5500')
+});
